fb25494ba234594c6ac6eee655bfd0b024a7705b
liurantao <liurantao@huawei.com>
2023-10-20 09:09:45 +0000

    update install.sh.
    Signed-off-by: liurantao <liurantao@huawei.com>

--- install.sh
+++ install.sh
@@ -14,14 +14,14 @@
 
 set -e
 {
-if [ "$(uname)" != "Darwin" ]; then
+if [ "$(uname)" != "Darwin" ];then
     flock -x 100
 fi
 if [ -d "$1/libusb-1.0.26" ];then
     rm -rf $1/libusb-1.0.26
 fi
 tar jxvf $2/libusb-1.0.26.tar.bz2 -C $1
-if [ "$(uname)" != "Darwin" ]; then
+if [ "$(uname)" != "Darwin" ];then
     flock -u 100
 fi
 } 100<>$2/lock_file.lock
